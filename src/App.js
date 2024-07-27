018d71cbc8ae0cb8036dd45aed3649c53da7896b
priest-2105 <fawzybailey782@gmail.com>
2024-07-27 01:26:18 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AppView from './views/AppView';
+ 
+function App () {
+  return ( 
+    <React.Fragment>
+      <AppView />
+    </React.Fragment>
+ );
+}
+ 
+export default App;
